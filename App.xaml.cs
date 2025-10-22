5001624bd71c577dafac6a36e0b83b1dc3dd616b
엄용호 <eyh05@naver.com>
2025-10-22 22:12:19 +0900

    수정

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Threading; 
 
 namespace CatchCapture;
 
@@ -9,5 +10,36 @@ namespace CatchCapture;
 /// </summary>
 public partial class App : Application
 {
-}
+    private static Mutex? _singleInstanceMutex;
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        bool createdNew;
+        // Global-named mutex to ensure single instance across user sessions as well
+        _singleInstanceMutex = new Mutex(initiallyOwned: true, name: "Global\\CatchCapture_SingleInstance", out createdNew);
+
+        if (!createdNew)
+        {
+            // Already running: inform the user and exit
+            MessageBox.Show("CatchCapture가 이미 실행 중입니다.", "CatchCapture", MessageBoxButton.OK, MessageBoxImage.Information);
+            Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            _singleInstanceMutex?.ReleaseMutex();
+            _singleInstanceMutex?.Dispose();
+        }
+        catch
+        {
+            // ignore
+        }
+        base.OnExit(e);
+    }
+}
