a6a7d44680b65a71fc0650876c9b4a3b8bace046
엄용호 <eyh05@naver.com>
2025-12-09 22:13:55 +0900

    수정

--- obj/Debug/net8.0-windows10.0.19041.0/CatchCapture.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/CatchCapture.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025 이지업소프트. All rights reserved.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("강력한 화면 캡처 및 편집 도구 - 영역 캡처, 스크롤 캡처, OCR, 실시간 편집 기능 제공")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd16cb4421b13a7fbd3f63ecaac41ee5fa34db1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18b5c6e24db4856b424783476b3b4c846ebf03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatchCapture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatchCapture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
