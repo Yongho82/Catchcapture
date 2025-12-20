ddf97b171f39740de3df7924780405ee8bb0a84a
엄용호 <eyh05@naver.com>
2025-12-21 00:15:20 +0900

    테마설정 도입

--- obj/Debug/net8.0-windows10.0.19041.0/en/CatchCapture.resources.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/en/CatchCapture.resources.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025 이지업소프트. All rights reserved.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("강력한 화면 캡처 및 편집 도구 - 영역 캡처, 스크롤 캡처, OCR, 실시간 편집 기능 제공")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+976f54aed70f2530a58e5c447c6ba9d46e5391de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522ef1667d2264065cd95e40b05ec47633c9f91a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatchCapture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatchCapture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
